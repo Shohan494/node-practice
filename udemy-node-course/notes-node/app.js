fc4bf695210db0dcf21f64393cde8a58f58733fc
shohan494 <shohan12224943@gmail.com>
2017-06-01 00:14:32 +0600

    learned to use relative require, arrow function as exported module function

---
+++ udemy-node-course/notes-node/app.js
@@ -0,0 +1,34 @@
+console.log("Starting app.js");
+
+// module require
+const fs = require("fs");
+const os = require("os");
+
+// relative require
+const notes = require("./notes.js");
+
+// calling function that has been exported
+
+console.log('Result:', notes.add(2, -3));
+
+// var user = os.userInfo();
+
+// console.log(user);
+
+// way 1.0 (ES6)
+// fs.appendFile('greetings.txt',`Hello ${user.username}! ` + `You are ${notes.age}.`);
+
+// way 1.1
+// fs.appendFile('greetings.txt', 'Hello ' + user.username + '! ');
+
+/* way 2
+fs.appendFile('greetings.txt', 'Hello World!', function (err){
+if (err){
+	console.log("Unable to write to file");
+	  }
+});
+*/
+
+/* way 3
+fs.appendFileSync ('greetings.txt', 'Hello World');
+*/
---
+++ udemy-node-course/notes-node/notes.js
@@ -0,0 +1,11 @@
+console.log("Starting notes.js");
+
+// console.log(module);
+
+// module.exports.age = 25;
+
+// arrow function
+module.exports.add = (a, b) => {
+	console.log("Adding Function");
+	return a + b;
+};
