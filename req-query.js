a8f84924973143bcd6c56d7e3e795495311c0309
shohan494 <shohan12224943@gmail.com>
2017-05-31 16:08:23 +0600

    more practice on upload, request query etc

---
+++ req-query.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(3000);
+
+// try this example with this url: 
+// http://localhost:8080/?year=2017&month=July
