109d066a1c1837b86bd1e32c62df6dc5bb09ca8d
shohan494 <shohan12224943@gmail.com>
2017-06-03 05:13:47 +0600

    note adding function works great but accepts duplicates

--- udemy-node-course/notes-node/notes.js
+++ udemy-node-course/notes-node/notes.js
@@ -1,7 +1,5 @@
 console.log("Starting notes.js");
-
 // console.log(module);
-
 // module.exports.age = 25;
 
 // arrow function
@@ -11,20 +9,29 @@ module.exports.add = (a, b) => {
 };
 
 const fs = require('fs');
-
 // yargs work
 // works in cli command : node index add --title=secret --body="This is my secret"    
 var addNote = (title, body) => {
-	console.log('Adding note', title, body);
+	console.log('Adding Note Function has been Called');
 	var notes = [];
 	var note = {
 		title,
 		body
+	};
+
+	try {
+		// important task is happening here
+		// each time it has been called and storing the notes each 
+		// time with reading and then storing the file 
+		var noteAsString = fs.readFileSync('notebook.json');
+		notes = JSON.parse(noteAsString);
+
+	} catch(e) {
+
 	}
 
-  notes.push(note);
+	notes.push(note);
   fs.writeFileSync('notebook.json', JSON.stringify(notes));
-
 };
 
 var getAll = () => {
